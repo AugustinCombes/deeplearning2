9cdfaf4b39bedf050e913ad9fcb905f5cdad7a10
thibautvalour <74783392+thibautvalour@users.noreply.github.com>
2023-03-21 02:36:36 +0100

    DNN

--- data/alphadigits.py
+++ data/alphadigits.py
@@ -4,11 +4,12 @@ import numpy as np
 import torch
 import string
 
-chars = string.digits+string.ascii_lowercase
+chars = string.digits + string.ascii_lowercase
 idx2char, char2idx = dict(), dict()
-for idx,char in enumerate(chars):
+for idx, char in enumerate(chars):
     idx2char[idx] = char
     char2idx[char] = idx
+
 
 class BinaryAlphaDigitsDataset(torch.utils.data.Dataset):
     def __init__(self, path_documents='data/rawdata/binaryalphadigs.mat', mode="all", restrict_labels=False):
@@ -25,16 +26,18 @@ class BinaryAlphaDigitsDataset(torch.utils.data.Dataset):
 
         x, y = data.shape
         labels = np.arange(x).T.reshape(-1, 1).repeat(y, axis=-1)
-        
+
         # restrict data to train or test set
         assert mode in ["train", "test", "all"]
-        
+
         if mode != "all":
             n_sample = data.shape[1]
-            train_data, test_data = data[:int(0.8*n_sample)], data[int(0.8*n_sample):]
-            train_labels, test_labels = labels[:int(0.8*n_sample)], labels[int(0.8*n_sample):]
-            data = train_data if mode =="train" else test_data
-            labels = train_labels if mode=="train" else test_labels
+            train_data, test_data = data[:int(
+                0.8*n_sample)], data[int(0.8*n_sample):]
+            train_labels, test_labels = labels[:int(
+                0.8*n_sample)], labels[int(0.8*n_sample):]
+            data = train_data if mode == "train" else test_data
+            labels = train_labels if mode == "train" else test_labels
 
         self.data = np.stack(data.reshape(-1))
         self.labels = np.stack(labels.reshape(-1))
